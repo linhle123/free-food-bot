a083fb39d099521ba9f57516f0673a178ec4ed1a
Linh Le <honglinh.vt09@gmail.com>
2018-01-02 23:26:15 -0600

    actually still need import statements

--- getdata.py
+++ getdata.py
@@ -1,11 +1,11 @@
 # from urllib.request import urlopen
 # from bs4 import BeautifulSoup
-# from selenium import webdriver
-# from selenium.webdriver.chrome.options import Options
-# from selenium.webdriver.common.by import By
-# from selenium.webdriver.support.ui import WebDriverWait
-# from selenium.webdriver.support import expected_conditions as EC
-# import time
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import time
 
 def get_sample_events():
     return "sample event yo"
