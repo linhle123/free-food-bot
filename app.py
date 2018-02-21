9d5e88c503ed31d94ec3165caa45f507aec49ad6
Linh Le <honglinh.vt09@gmail.com>
2018-02-20 21:36:15 -0600

    delete an extra line that cause key error

--- app.py
+++ app.py
@@ -49,7 +49,7 @@ def webhook():
 
                     sender_id = messaging_event["sender"]["id"]        # the facebook ID of the person sending you the message
                     recipient_id = messaging_event["recipient"]["id"]  # the recipient's ID, which should be your page's facebook ID
-                    message_text = messaging_event["message"]["text"]  # the message's text
+                    # message_text = messaging_event["message"]["text"]  # the message's text
                     
                     message = messaging_event["message"]
                     payload = ""
